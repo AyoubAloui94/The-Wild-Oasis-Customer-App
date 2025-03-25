00e66f0492de1bc0e7da4522b797ad1ad25c466d
Ayoub Aloui <webitgenius@gmail.com>
2025-03-26 00:12:58 +0100

    minor detail

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -16,11 +16,9 @@ export default async function Page({ searchParams }) {
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
       <p className="text-primary-200 text-lg mb-10">Cozy yet luxurious cabins, located right in the heart of the Italian Dolomites. Imagine waking up to beautiful mountain views, spending your days exploring the dark forests around, or just relaxing in your private hot tub under the stars. Enjoy nature&apos;s beauty in your own little home away from home. The perfect spot for a peaceful, calm vacation. Welcome to paradise.</p>
-
       <div className="flex justify-end mb-8">
         <Filter />
       </div>
-
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
         <ReservationReminder />
